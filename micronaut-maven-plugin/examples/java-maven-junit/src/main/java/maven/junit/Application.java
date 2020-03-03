492b37f1b854f48494f3342a3b2d59e7c4925085
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-03-03 12:59:24 +0100

    Sample app changes

--- micronaut-maven-plugin/examples/java-maven-junit/src/main/java/maven/junit/Application.java
+++ micronaut-maven-plugin/examples/java-maven-junit/src/main/java/maven/junit/Application.java
@@ -1,10 +1,12 @@
 package maven.junit;
 
 import io.micronaut.runtime.Micronaut;
+import org.apache.commons.text.WordUtils;
 
 public class Application {
 
     public static void main(String[] args) {
+//        System.out.println(WordUtils.capitalize("asdasd"));
         Micronaut.run(Application.class);
     }
 }
