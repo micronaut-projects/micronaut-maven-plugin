bd2aa84adb69c396d56240fed2ddfbac87a539e8
Álvaro Sánchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-10-09 16:21:13 +0200

    Move the docker-native support away from Jib and into its own mojo

---
+++ src/main/java/io/micronaut/build/DockerNativeMojo.java
@@ -0,0 +1,130 @@
+package io.micronaut.build;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.BuildImageCmd;
+import com.github.dockerjava.api.command.BuildImageResultCallback;
+import com.github.dockerjava.api.model.BuildResponseItem;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.transport.DockerHttpClient;
+import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
+
+/**
+ * TODO: javadoc
+ *
+ * @author Álvaro Sánchez-Mariscal
+ * @since 1.0.0
+ */
+@Mojo(name = DockerNativeMojo.DOCKER_NATIVE_PACKAGING)
+public class DockerNativeMojo extends AbstractMojo {
+
+    public static final String DOCKER_NATIVE_PACKAGING = "docker-native";
+    private final MavenProject mavenProject;
+
+    /**
+     * The project's target directory.
+     */
+    @Parameter(defaultValue = "${project.build.directory}")
+    private File targetDirectory;
+
+    /**
+     * The main class of the application, as defined in the
+     * <a href="https://www.mojohaus.org/exec-maven-plugin/java-mojo.html#mainClass">Exec Maven Plugin</a>.
+     */
+    @Parameter(defaultValue = "${exec.mainClass}", required = true)
+    private String mainClass;
+
+    private final DockerClient dockerClient;
+
+    @Inject
+    public DockerNativeMojo(MavenProject mavenProject, MavenSession mavenSession) {
+        this.mavenProject = mavenProject;
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerHttpClient httpClient = new ZerodepDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .build();
+        dockerClient = DockerClientImpl.getInstance(config, httpClient);
+    }
+
+
+    @Override
+    public void execute() {
+        String packaging = mavenProject.getPackaging();
+
+        if (DOCKER_NATIVE_PACKAGING.equals(packaging)) {
+            try {
+                copyDependencies();
+                File dockerfile = loadDockerfileAsResource("/dockerfiles/DockerfileNative");
+
+                BuildImageResultCallback resultCallback = new BuildImageResultCallback() {
+                    @Override
+                    public void onNext(BuildResponseItem item) {
+                        super.onNext(item);
+
+                        if (item.isErrorIndicated()) {
+                            getLog().error(item.getErrorDetail().getMessage());
+                        } else {
+                            getLog().info(StringUtils.chomp(item.getStream(), "\n"));
+                        }
+                    }
+                };
+
+                //TODO check Jib plugin config?
+                Set<String> tags = new HashSet<>(Collections.singletonList(mavenProject.getName()));
+
+                String imageId = dockerClient.buildImageCmd(dockerfile)
+                        //TODO allow to customize, check Jib plugin config
+                        //TODO check Java version
+                        .withBuildArg("BASE_IMAGE", "oracle/graalvm-ce:20.2.0-java11")
+                        .withBuildArg("CLASS_NAME", mainClass)
+                        .withTags(tags)
+                        .exec(resultCallback)
+                        .awaitImageId();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            getLog().warn("Skipping. POM packaging should be set to " + DOCKER_NATIVE_PACKAGING);
+        }
+
+    }
+
+    private File loadDockerfileAsResource(String path) throws IOException {
+        InputStream stream = getClass().getResourceAsStream(path);
+        if (stream != null) {
+            String fileName = path.substring(path.lastIndexOf("/") + 1);
+            File dockerfile = new File(targetDirectory, fileName);
+            FileUtils.copyInputStreamToFile(stream, dockerfile);
+            return dockerfile;
+        }
+        return null;
+    }
+
+    private void copyDependencies() throws IOException {
+        List<String> imageClasspathScopes = Arrays.asList(Artifact.SCOPE_COMPILE, Artifact.SCOPE_RUNTIME);
+        mavenProject.setArtifactFilter(artifact -> imageClasspathScopes.contains(artifact.getScope()));
+        File target = new File(mavenProject.getBuild().getDirectory(), "dependency");
+        target.mkdirs();
+        for (Artifact dependency : mavenProject.getArtifacts()) {
+            Files.copy(dependency.getFile().toPath(), target.toPath().resolve(dependency.getFile().getName()), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
--- src/main/java/io/micronaut/build/jib/JibMicronautExtension.java
+++ src/main/java/io/micronaut/build/jib/JibMicronautExtension.java
@@ -1,42 +1,16 @@
 package io.micronaut.build.jib;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.BuildImageResultCallback;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.model.BuildResponseItem;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.transport.DockerHttpClient;
-import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
 import com.google.cloud.tools.jib.api.buildplan.*;
 import com.google.cloud.tools.jib.buildplan.UnixPathParser;
 import com.google.cloud.tools.jib.maven.extension.JibMavenPluginExtension;
 import com.google.cloud.tools.jib.maven.extension.MavenData;
-import com.google.cloud.tools.jib.maven.extension.nativeimage.JibNativeImageExtension;
 import com.google.cloud.tools.jib.plugins.extension.ExtensionLogger;
 import com.google.cloud.tools.jib.plugins.extension.JibPluginExtensionException;
 import io.micronaut.build.MicronautRuntime;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
-import org.apache.commons.compress.utils.IOUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
-import org.apache.maven.project.MavenProject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.*;
-
-import static com.google.cloud.tools.jib.plugins.extension.ExtensionLogger.LogLevel.ERROR;
-import static com.google.cloud.tools.jib.plugins.extension.ExtensionLogger.LogLevel.LIFECYCLE;
 
 /**
  * TODO: javadoc
@@ -45,19 +19,6 @@ import static com.google.cloud.tools.jib.plugins.extension.ExtensionLogger.LogLe
  * @since 1.0.0
  */
 public class JibMicronautExtension implements JibMavenPluginExtension<Void> {
-
-    private final JibNativeImageExtension nativeImageExtension;
-    private final DockerClient dockerClient;
-
-    public JibMicronautExtension() {
-        nativeImageExtension = new JibNativeImageExtension();
-        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
-        DockerHttpClient httpClient = new ZerodepDockerHttpClient.Builder()
-                .dockerHost(config.getDockerHost())
-                .sslConfig(config.getSSLConfig())
-                .build();
-        dockerClient = DockerClientImpl.getInstance(config, httpClient);
-    }
 
     @Override
     public Optional<Class<Void>> getExtraConfigType() {
@@ -70,83 +31,34 @@ public class JibMicronautExtension implements JibMavenPluginExtension<Void> {
                                                        ExtensionLogger logger) throws JibPluginExtensionException {
 
         ContainerBuildPlan.Builder builder = buildPlan.toBuilder();
-        String packaging = mavenData.getMavenProject().getPackaging();
-        if ("docker-native".equals(packaging)) {
-            logger.log(LIFECYCLE, "Will generate a Native Image inside a Docker image");
-            try {
-                copyDependencies(mavenData.getMavenProject());
+        //TODO make a best-effort guess
+        MicronautRuntime runtime = MicronautRuntime.valueOf(properties.getOrDefault("micronautRuntime", "NONE"));
 
-                String targetDir = mavenData.getMavenProject().getBuild().getDirectory();
-                File dockerfile = new File(targetDir, "Dockerfile");
-                //TODO publish image
-                FileUtils.write(dockerfile, "FROM alvarosanchez/micronaut-native-image-builder:20.2.0-java11", Charset.defaultCharset());
+        //TODO be able to tell if the user has configured a from - read plugin configuration using Maven API?
+        builder.setBaseImage("openjdk:14-alpine")
+                //TODO detect ports
+                .addExposedPort(Port.tcp(8080));
 
-                BuildImageResultCallback resultCallback = new BuildImageResultCallback() {
-                    @Override
-                    public void onNext(BuildResponseItem item) {
-                        super.onNext(item);
+        switch (runtime.getBuildStrategy()) {
+            case ORACLE_FUNCTION:
 
-                        if (item.isErrorIndicated()) {
-                            logger.log(ERROR, item.getErrorDetail().getMessage());
-                        } else {
-                            logger.log(LIFECYCLE, StringUtils.chomp(item.getStream(), "\n"));
-                        }
-                    }
-                };
+                List<? extends LayerObject> originalLayers = buildPlan.getLayers();
+                builder.setLayers(Collections.emptyList());
 
-                String imageId = dockerClient.buildImageCmd(dockerfile)
-                        .withBuildArg("className", Objects.requireNonNull(buildPlan.getEntrypoint()).get(3))
-                        .exec(resultCallback)
-                        .awaitImageId();
-
-                CreateContainerResponse container = dockerClient.createContainerCmd(imageId).exec();
-                dockerClient.startContainerCmd(container.getId());
-                InputStream nativeImage = dockerClient.copyArchiveFromContainerCmd(container.getId(), "/home/app/application").exec();
-
-                try (TarArchiveInputStream fin = new TarArchiveInputStream(nativeImage)) {
-                    TarArchiveEntry tarEntry = fin.getNextTarEntry();
-                    IOUtils.copy(fin, new FileOutputStream(new File(targetDir, tarEntry.getName())));
+                for (LayerObject layer : originalLayers) {
+                    builder.addLayer(remapLayer(layer));
                 }
 
-                //TODO publish base image
-                builder.setBaseImage("alvarosanchez/micronaut-docker-native:latest")
-                        .setLayers(Collections.emptyList());
-
-                properties.putIfAbsent("imageName", "application");
-                return nativeImageExtension.extendContainerBuildPlan(builder.build(), properties, Optional.empty(), mavenData, logger);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            //TODO make a best-effort guess
-            MicronautRuntime runtime = MicronautRuntime.valueOf(properties.getOrDefault("micronautRuntime", "NONE"));
-
-            //TODO be able to tell if the user has configured a from - read plugin configuration using Maven API?
-            builder.setBaseImage("openjdk:14-alpine")
-                    //TODO detect ports
-                    .addExposedPort(Port.tcp(8080));
-
-            switch (runtime.getBuildStrategy()) {
-                case ORACLE_FUNCTION:
-
-                    List<? extends LayerObject> originalLayers = buildPlan.getLayers();
-                    builder.setLayers(Collections.emptyList());
-
-                    for (LayerObject layer : originalLayers) {
-                        builder.addLayer(remapLayer(layer));
-                    }
-
-                    builder.setBaseImage("fnproject/fn-java-fdk:" + determineProjectFnVersion())
-                            .setWorkingDirectory(AbsoluteUnixPath.get("/function"))
-                            .setEntrypoint(null)
-                            .setCmd(Collections.singletonList("io.micronaut.oraclecloud.function.http.HttpFunction::handleRequest"));
-                    break;
-                case LAMBDA:
-                    List<String> entrypoint = buildPlan.getEntrypoint();
-                    Objects.requireNonNull(entrypoint).set(entrypoint.size() - 1, "io.micronaut.function.aws.runtime.MicronautLambdaRuntime");
-                    builder.setEntrypoint(entrypoint);
-                    break;
-            }
+                builder.setBaseImage("fnproject/fn-java-fdk:" + determineProjectFnVersion())
+                        .setWorkingDirectory(AbsoluteUnixPath.get("/function"))
+                        .setEntrypoint(null)
+                        .setCmd(Collections.singletonList("io.micronaut.oraclecloud.function.http.HttpFunction::handleRequest"));
+                break;
+            case LAMBDA:
+                List<String> entrypoint = buildPlan.getEntrypoint();
+                Objects.requireNonNull(entrypoint).set(entrypoint.size() - 1, "io.micronaut.function.aws.runtime.MicronautLambdaRuntime");
+                builder.setEntrypoint(entrypoint);
+                break;
         }
         return builder.build();
     }
@@ -175,16 +87,6 @@ public class JibMicronautExtension implements JibMavenPluginExtension<Void> {
             return "jre11-latest";
         } else {
             return "latest";
-        }
-    }
-
-    private void copyDependencies(MavenProject project) throws IOException {
-        List<String> imageClasspathScopes = Arrays.asList(Artifact.SCOPE_COMPILE, Artifact.SCOPE_RUNTIME);
-        project.setArtifactFilter(artifact -> imageClasspathScopes.contains(artifact.getScope()));
-        File target = new File(project.getBuild().getDirectory(), "dependency");
-        target.mkdirs();
-        for (Artifact dependency : project.getArtifacts()) {
-            Files.copy(dependency.getFile().toPath(), target.toPath().resolve(dependency.getFile().getName()));
         }
     }
 
