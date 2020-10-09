9f977372f0252140ef4af5156a8660b22e1454ec
Álvaro Sánchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-10-09 18:18:02 +0200

    Read Jib plugin configuration for generating the docker-native image

--- src/main/java/io/micronaut/build/DockerNativeMojo.java
+++ src/main/java/io/micronaut/build/DockerNativeMojo.java
@@ -1,7 +1,6 @@
 package io.micronaut.build;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.BuildImageCmd;
 import com.github.dockerjava.api.command.BuildImageResultCallback;
 import com.github.dockerjava.api.model.BuildResponseItem;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
@@ -9,10 +8,12 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.transport.DockerHttpClient;
 import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+import io.micronaut.build.services.JibConfigurationService;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.execution.MavenSession;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -36,7 +37,10 @@ import java.util.*;
 public class DockerNativeMojo extends AbstractMojo {
 
     public static final String DOCKER_NATIVE_PACKAGING = "docker-native";
+    public static final String DEFAULT_GRAAL_VERSION = "20.2.0";
+    public static final String DEFAULT_GRAAL_JAVA_VERSION = "java11";
     private final MavenProject mavenProject;
+    private final JibConfigurationService jibConfigurationService;
 
     /**
      * The project's target directory.
@@ -53,9 +57,11 @@ public class DockerNativeMojo extends AbstractMojo {
 
     private final DockerClient dockerClient;
 
+    @SuppressWarnings("CdiInjectionPointsInspection")
     @Inject
-    public DockerNativeMojo(MavenProject mavenProject, MavenSession mavenSession) {
+    public DockerNativeMojo(MavenProject mavenProject, JibConfigurationService jibConfigurationService) {
         this.mavenProject = mavenProject;
+        this.jibConfigurationService = jibConfigurationService;
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
         DockerHttpClient httpClient = new ZerodepDockerHttpClient.Builder()
                 .dockerHost(config.getDockerHost())
@@ -78,7 +84,6 @@ public class DockerNativeMojo extends AbstractMojo {
                     @Override
                     public void onNext(BuildResponseItem item) {
                         super.onNext(item);
-
                         if (item.isErrorIndicated()) {
                             getLog().error(item.getErrorDetail().getMessage());
                         } else {
@@ -87,13 +92,14 @@ public class DockerNativeMojo extends AbstractMojo {
                     }
                 };
 
-                //TODO check Jib plugin config?
-                Set<String> tags = new HashSet<>(Collections.singletonList(mavenProject.getName()));
+                Set<String> tags = new HashSet<>(Collections.singletonList(jibConfigurationService.getToImage().orElse(mavenProject.getArtifactId())));
+                tags.addAll(jibConfigurationService.getTags());
 
-                String imageId = dockerClient.buildImageCmd(dockerfile)
-                        //TODO allow to customize, check Jib plugin config
-                        //TODO check Java version
-                        .withBuildArg("BASE_IMAGE", "oracle/graalvm-ce:20.2.0-java11")
+                String from = jibConfigurationService.getFromImage().orElse(determineBaseImage());
+                getLog().info("Using base image: " + from);
+
+                dockerClient.buildImageCmd(dockerfile)
+                        .withBuildArg("BASE_IMAGE", from)
                         .withBuildArg("CLASS_NAME", mainClass)
                         .withTags(tags)
                         .exec(resultCallback)
@@ -105,6 +111,16 @@ public class DockerNativeMojo extends AbstractMojo {
             getLog().warn("Skipping. POM packaging should be set to " + DOCKER_NATIVE_PACKAGING);
         }
 
+    }
+
+    private String determineBaseImage() {
+        String graalVersion = mavenProject.getProperties().getProperty("graal.version", DEFAULT_GRAAL_VERSION);
+        ArtifactVersion javaVersion = new DefaultArtifactVersion(Optional.ofNullable(mavenProject.getProperties().getProperty("maven.compiler.target")).orElse(System.getProperty("java.version")));
+        String image = "oracle/graalvm-ce:" + graalVersion;
+        if (javaVersion.getMajorVersion() >= 11) {
+            image += "-" + DEFAULT_GRAAL_JAVA_VERSION;
+        }
+        return image;
     }
 
     private File loadDockerfileAsResource(String path) throws IOException {
