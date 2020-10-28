cea3eebfe15c00d8f2d5cc5811bd597cc99436d3
Álvaro Sánchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-10-28 09:43:21 +0100

    Cleanup

--- docker/native/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t alvarosanchez/micronaut-docker-native:latest .
-docker build -t alvarosanchez/micronaut-native-image-builder:20.2.0-java11 -f DockerfileBuild .
-docker push alvarosanchez/micronaut-native-image-builder:20.2.0-java11
-docker push alvarosanchez/micronaut-docker-native:latest
