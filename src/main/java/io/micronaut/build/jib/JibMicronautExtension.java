19fdac85147b3fa06deebb306416586ef94f3998
Iván López <lopezi@objectcomputing.com>
2021-04-27 08:54:02 +0200

    Use openjdk:16-alpine as base image in Docker

--- src/main/java/io/micronaut/build/jib/JibMicronautExtension.java
+++ src/main/java/io/micronaut/build/jib/JibMicronautExtension.java
@@ -23,7 +23,7 @@ import java.util.*;
  */
 public class JibMicronautExtension implements JibMavenPluginExtension<Void> {
 
-    public static final String DEFAULT_BASE_IMAGE = "openjdk:15-alpine";
+    public static final String DEFAULT_BASE_IMAGE = "openjdk:16-alpine";
 
     @Override
     public Optional<Class<Void>> getExtraConfigType() {
