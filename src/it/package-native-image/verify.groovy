9bb86d4061051e93e863e99417b74dcf4124dd44
Iván López <lopezi@objectcomputing.com>
2021-06-07 12:28:47 +0200

    New goal (executed automatically) to generate GraalVM resources file

--- src/it/package-native-image/verify.groovy
+++ src/it/package-native-image/verify.groovy
@@ -1,6 +1,7 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("native-image-maven-plugin:${nativeImagePluginVersion}:native-image")
+assert log.text.contains("micronaut-maven-plugin:${pluginVersion}:graalvm-resources")
 //assert log.text.contains("--no-fallback")
 //assert log.text.contains("-H:Class=io.micronaut.build.examples.Application")
 //assert log.text.contains("-H:Name=package-native-image")
