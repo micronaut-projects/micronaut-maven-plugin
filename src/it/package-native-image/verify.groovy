acc46d53714f3368e8e47efe17c1038b1d102676
Iván López <lopezi@objectcomputing.com>
2021-06-07 16:10:38 +0200

    Improve tests to check the content of the generated resource-config.json

--- src/it/package-native-image/verify.groovy
+++ src/it/package-native-image/verify.groovy
@@ -5,6 +5,12 @@ assert log.text.contains("micronaut-maven-plugin:${pluginVersion}:graalvm-resour
 //assert log.text.contains("--no-fallback")
 //assert log.text.contains("-H:Class=io.micronaut.build.examples.Application")
 //assert log.text.contains("-H:Name=package-native-image")
+File resourceConfigFile = new File(basedir, 'target/classes/META-INF/native-image/io.micronaut.build.examples/package-native-image/resource-config.json')
+def resourceConfigJson = new groovy.json.JsonSlurper().parse(resourceConfigFile)
+
+assert resourceConfigJson.resources.pattern.any { it == "\\Qapplication.yml\\E" }
+assert resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/app-0.0.yml\\E" }
+assert resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/views/swagger-ui/index.html\\E" }
 
 File fatJar = new File(basedir, "target/package-native-image-0.1.jar")
 assert fatJar.exists()
