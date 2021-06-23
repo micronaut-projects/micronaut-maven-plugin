4d954df4d6d55d4903c190951b5d3977c5bbec37
Iván López <lopezi@objectcomputing.com>
2021-06-23 13:30:31 +0200

    New goal (executed automatically) to generate GraalVM resources file (#184)
    * New goal (executed automatically) to generate GraalVM resources file
    
    * Move graalvm-resources goal to prepare-package phase
    
    * Improve tests to check the content of the generated resource-config.json
    
    * Add parameter to skip generation of resource config file

--- src/it/package-native-image/src/main/java/io/micronaut/build/examples/Application.java
+++ src/it/package-native-image/src/main/java/io/micronaut/build/examples/Application.java
@@ -3,6 +3,15 @@ package io.micronaut.build.examples;
 import io.micronaut.runtime.Micronaut;
 import io.micronaut.build.examples.greeter.Greeter;
 
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "app",
+            version = "0.0"
+    )
+)
 public class Application {
 
     public static void main(String[] args) {
--- src/it/package-native-image/verify.groovy
+++ src/it/package-native-image/verify.groovy
@@ -1,9 +1,16 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("native-image-maven-plugin:${nativeImagePluginVersion}:native-image")
+assert log.text.contains("micronaut-maven-plugin:${pluginVersion}:graalvm-resources")
 //assert log.text.contains("--no-fallback")
 //assert log.text.contains("-H:Class=io.micronaut.build.examples.Application")
 //assert log.text.contains("-H:Name=package-native-image")
+File resourceConfigFile = new File(basedir, 'target/classes/META-INF/native-image/io.micronaut.build.examples/package-native-image/resource-config.json')
+def resourceConfigJson = new groovy.json.JsonSlurper().parse(resourceConfigFile)
+
+assert resourceConfigJson.resources.pattern.any { it == "\\Qapplication.yml\\E" }
+assert resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/app-0.0.yml\\E" }
+assert resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/views/swagger-ui/index.html\\E" }
 
 File fatJar = new File(basedir, "target/package-native-image-0.1.jar")
 assert fatJar.exists()
