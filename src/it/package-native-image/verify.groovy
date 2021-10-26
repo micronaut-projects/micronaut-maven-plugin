c0454889e97a2f59b10cf0410c095dc62c0e43db
Iván López <lopezi@objectcomputing.com>
2021-10-26 13:17:44 +0200

    Upgrade plugin to GraalVM 21.3.0 and official GraalVM Maven plugin
    Fixes #232

--- src/it/package-native-image/verify.groovy
+++ src/it/package-native-image/verify.groovy
@@ -1,10 +1,8 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
-assert log.text.contains("native-image-maven-plugin:${nativeImagePluginVersion}:native-image")
+assert log.text.contains("native-maven-plugin:${nativeMavenPluginVersion}:build")
 assert log.text.contains("micronaut-maven-plugin:${pluginVersion}:graalvm-resources")
-//assert log.text.contains("--no-fallback")
-//assert log.text.contains("-H:Class=io.micronaut.build.examples.Application")
-//assert log.text.contains("-H:Name=package-native-image")
+
 File resourceConfigFile = new File(basedir, 'target/classes/META-INF/native-image/io.micronaut.build.examples/package-native-image/resource-config.json')
 def resourceConfigJson = new groovy.json.JsonSlurper().parse(resourceConfigFile)
 
