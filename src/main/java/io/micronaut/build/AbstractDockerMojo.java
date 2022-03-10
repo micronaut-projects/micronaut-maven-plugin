2e21c5d1a3df974a124be8383b5fbae7fba5ab7e
Tristan Roberts <tristan_roberts@icloud.com>
2022-03-10 23:20:09 +1100

    Fix #272

--- src/main/java/io/micronaut/build/AbstractDockerMojo.java
+++ src/main/java/io/micronaut/build/AbstractDockerMojo.java
@@ -111,9 +111,9 @@ public abstract class AbstractDockerMojo extends AbstractMojo {
     protected String getFrom() {
         if (staticNativeImage) {
             // For building a static native image we need a base image with tools (cc, make,...) already installed
-            return jibConfigurationService.getFromImage().orElse("ghcr.io/graalvm/graalvm-ce:" + graalVmJvmVersion() + "-" + graalVmVersion());
+            return jibConfigurationService.getFromImage().orElse("ghcr.io/graalvm/graalvm-ce:ol8-" + graalVmJvmVersion() + "-" + graalVmVersion());
         } else {
-            return jibConfigurationService.getFromImage().orElse("ghcr.io/graalvm/native-image:" + graalVmJvmVersion() + "-" + graalVmVersion());
+            return jibConfigurationService.getFromImage().orElse("ghcr.io/graalvm/native-image:ol8-" + graalVmJvmVersion() + "-" + graalVmVersion());
         }
     }
 
