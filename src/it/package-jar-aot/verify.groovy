7c66efbde35df93dc12fff8eca78f7a0edce3fb4
Alvaro Sanchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-03-15 18:33:57 +0100

    Fix/remove tests

--- src/it/package-jar-aot/verify.groovy
+++ src/it/package-jar-aot/verify.groovy
@@ -5,7 +5,7 @@ assert log.exists()
 assert log.text.contains("Replacing original artifact with shaded artifact")
 assert log.text.contains("Running Micronaut AOT")
 
-File fatJar = new File(basedir, "target/package-jar-0.1.jar")
+File fatJar = new File(basedir, "target/package-jar-aot-0.1.jar")
 assert fatJar.exists()
 
 JarFile jarFile = new JarFile(fatJar)
