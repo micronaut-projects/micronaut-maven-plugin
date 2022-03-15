ab959a58ba3bd7bf29daa1fb0a49625b396083b2
Alvaro Sanchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-03-15 15:35:14 +0100

    Add test for AOT package

---
+++ src/it/package-jar-aot/verify.groovy
@@ -0,0 +1,12 @@
+import java.util.jar.JarFile
+
+File log = new File(basedir, 'build.log')
+assert log.exists()
+assert log.text.contains("Replacing original artifact with shaded artifact")
+assert log.text.contains("Running Micronaut AOT")
+
+File fatJar = new File(basedir, "target/package-jar-0.1.jar")
+assert fatJar.exists()
+
+JarFile jarFile = new JarFile(fatJar)
+assert jarFile.stream().anyMatch{ e -> e.name == 'io/micronaut/build/examples/generated/AOTApplicationContextConfigurer.class' }
