831e42e009e9ac1f0b61f6ab0ed6e391ad91bebd
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-07-21 15:56:33 +0200

    Make the build work with GraalVM 22.2 (#427)

--- src/it/dockerfile-docker-native-lambda/verify.groovy
+++ src/it/dockerfile-docker-native-lambda/verify.groovy
@@ -1,4 +1,13 @@
 File dockerfile = new File("$basedir/target", "Dockerfile")
-File expectedDockerfile = new File(basedir, "Dockerfile")
+File expectedDockerfile = new File(basedir, "Dockerfile.${graalVmArch()}")
 
 assert dockerfile.text == expectedDockerfile.text
+
+static String graalVmArch() {
+    String osArch = System.getProperty("os.arch");
+    if ("aarch64".equals(osArch)) {
+        return "aarch64";
+    } else {
+        return "amd64";
+    }
+}
