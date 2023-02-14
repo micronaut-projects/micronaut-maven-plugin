4c788e06c94a8a4a0b5a386d809fffe16773f749
alvarosanchez <alvarosanchez@users.noreply.github.com>
2023-02-14 16:31:28 +0000

    Deploying to gh-pages - 16:31:28

---
+++ snapshot/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.maven"},{"l":"io.micronaut.maven.aot"},{"l":"io.micronaut.maven.jib"},{"l":"io.micronaut.maven.micronaut_maven_plugin"},{"l":"io.micronaut.maven.services"},{"l":"io.micronaut.maven.testresources"}];updateSearchResults();
