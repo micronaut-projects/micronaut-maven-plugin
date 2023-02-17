32baecd487351c772c7b823bf5d35081a669707d
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-02-17 08:40:24 +0100

    Fix build

--- src/it/dockerfile-docker-oracle-function/verify.groovy
+++ src/it/dockerfile-docker-oracle-function/verify.groovy
@@ -1,7 +1,4 @@
 File dockerfile = new File("$basedir/target", "Dockerfile")
 File expectedDockerfile = new File(basedir, "Dockerfile")
 def javaVersion = System.getProperty("java.specification.version")
-if (javaVersion == "1.8") {
-    expectedDockerfile = new File(basedir, "Dockerfile.java8")
-}
 assert dockerfile.text == expectedDockerfile.text
