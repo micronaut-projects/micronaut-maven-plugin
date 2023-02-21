eff9043d0539896b01d04954dddf233a14c1c44d
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-02-21 17:40:07 +0100

    Add positive test

--- micronaut-maven-plugin/src/it/snakeyaml-missing/verify.groovy
+++ micronaut-maven-plugin/src/it/snakeyaml-missing/verify.groovy
@@ -1,4 +1,5 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("BUILD FAILURE")
+assert log.text.contains("Rule 2: io.micronaut.maven.enforcer.CheckSnakeYaml failed with message")
 assert log.text.contains("YAML configuration file detected, but SnakeYAML is not on the runtime classpath. Make sure to add the following dependency:")
