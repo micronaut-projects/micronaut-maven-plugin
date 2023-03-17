7df92dacb75c7d17b4a6eada9b49230df6fe7321
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-03-17 15:15:52 +0100

    Verify that the Docker daemon is running
    Fixes #668

---
+++ micronaut-maven-integration-tests/src/it/package-docker-down/src/main/java/io/micronaut/build/examples/Application.java
@@ -0,0 +1,8 @@
+package io.micronaut.build.examples;
+
+import io.micronaut.runtime.Micronaut;
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
