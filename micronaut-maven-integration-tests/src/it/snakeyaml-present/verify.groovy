7d40c61595e90e3b5b889f0e5db7919061d95b7d
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-03-21 22:40:38 +0100

    Fix build

--- micronaut-maven-integration-tests/src/it/snakeyaml-present/verify.groovy
+++ micronaut-maven-integration-tests/src/it/snakeyaml-present/verify.groovy
@@ -1,4 +1,4 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("BUILD SUCCESS")
-assert log.text.contains("Rule 2: io.micronaut.maven.enforcer.CheckSnakeYaml passed")
+assert log.text.contains("io.micronaut.maven.enforcer.CheckSnakeYaml passed")
