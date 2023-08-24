1a0adaf952dda0c157aa8151ee1e6706164cf63b
Andriy Dmytruk <80816836+andriy-dmytruk@users.noreply.github.com>
2023-08-24 07:24:51 -0400

    Fixes for the native image build (#822)
    * Upgrade Zlib to 1.3
    
    * Fixes for the native image build
    
    * Fix the reading of .args file. If it contains an argument
      starting with '@', it references another .arg file that
      requires to be read. The arguments from the new file are
      also processed.
    * Use newer version of alpine-glibc for BASE_IMAGE_RUN
      as glibc 2.34 is required for native image 17 executables.
    
    * Update integration tests
    
    * Windows fix
    
    Fix the native image args on Windows that was caused by
    different file separators used.
    
    ---------
    
    Co-authored-by: Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>

--- micronaut-maven-integration-tests/src/it/package-docker-native/verify.groovy
+++ micronaut-maven-integration-tests/src/it/package-docker-native/verify.groovy
@@ -1,5 +1,5 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("mn:${pluginVersion}:graalvm-resources")
-assert log.text.contains("Using BASE_IMAGE_RUN: frolvlad/alpine-glibc:alpine-3.12")
+assert log.text.contains("Using BASE_IMAGE_RUN: frolvlad/alpine-glibc:glibc-2.34")
 assert log.text.contains("Successfully tagged alvarosanchez/demo:0.1")
