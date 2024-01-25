56b5b308abcf2fe3bb00e28ad4c18bbbe10c081c
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-01-25 18:09:21 +0100

    Another test

--- micronaut-maven-integration-tests/src/it/dockerfile-docker-native-netty/verify.groovy
+++ micronaut-maven-integration-tests/src/it/dockerfile-docker-native-netty/verify.groovy
@@ -3,3 +3,8 @@ File expectedDockerfile = new File(basedir, "Dockerfile")
 String expectedDockerfileText = expectedDockerfile.text.replace("17", "${System.getProperty("java.specification.version")}")
 
 assert dockerfile.text == expectedDockerfileText
+
+File log = new File(basedir, 'build.log')
+
+assert log.exists()
+assert log.text.contains("io.micronaut.runtime.Micronaut - Startup completed")
