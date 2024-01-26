33f276e689058301bb28b7d7f278eae619924707
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-01-26 16:17:28 +0100

    Test OCI function dockerfiles

--- micronaut-maven-integration-tests/src/it/dockerfile-docker-oracle-function/verify.groovy
+++ micronaut-maven-integration-tests/src/it/dockerfile-docker-oracle-function/verify.groovy
@@ -1,3 +1,10 @@
 File dockerfile = new File("$basedir/target", "Dockerfile")
 File expectedDockerfile = new File(basedir, "Dockerfile")
-assert dockerfile.text == expectedDockerfile.text
+String expectedDockerfileText = expectedDockerfile.text.replace("17", "${System.getProperty("java.specification.version")}")
+
+assert dockerfile.text == expectedDockerfileText
+
+File log = new File(basedir, 'build.log')
+assert log.exists()
+assert log.text.contains("fnproject/fn-java-fdk:jre17-latest")
+assert log.text.contains("Successfully tagged alvarosanchez/dockerfile-docker-oracle-function:0.1")
