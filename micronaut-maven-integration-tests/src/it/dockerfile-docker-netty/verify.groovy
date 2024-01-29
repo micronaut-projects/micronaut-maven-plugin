8355e0f70ae4b3a661fe1af6168ad13b7912e75a
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-01-29 12:23:50 +0100

    Remove duplicate test

--- micronaut-maven-integration-tests/src/it/dockerfile-docker-netty/verify.groovy
+++
@@ -1,5 +0,0 @@
-File dockerfile = new File("$basedir/target", "Dockerfile")
-File expectedDockerfile = new File(basedir, "Dockerfile")
-String expectedDockerfileText = expectedDockerfile.text.replace("17", "${System.getProperty("java.specification.version")}")
-
-assert dockerfile.text == expectedDockerfileText
