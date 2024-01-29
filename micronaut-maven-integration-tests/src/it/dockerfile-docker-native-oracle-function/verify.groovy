31db7ac6233075e3eb532d8bb9858c6c293cf1d4
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-01-29 12:35:18 +0100

    Fix test

--- micronaut-maven-integration-tests/src/it/dockerfile-docker-native-oracle-function/verify.groovy
+++ micronaut-maven-integration-tests/src/it/dockerfile-docker-native-oracle-function/verify.groovy
@@ -3,3 +3,7 @@ File expectedDockerfile = new File(basedir, "Dockerfile")
 String expectedDockerfileText = expectedDockerfile.text.replace("17-ol9", "${System.getProperty("java.specification.version")}-ol9")
 
 assert dockerfile.text == expectedDockerfileText
+
+File log = new File(basedir, 'build.log')
+assert log.exists()
+assert log.text.contains("Successfully tagged alvarosanchez/dockerfile-docker-native-oracle-function:0.1")
