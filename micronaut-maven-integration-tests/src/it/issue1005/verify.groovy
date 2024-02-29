44f1e99050395b92767afcf973418f6ddfa51293
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-02-29 13:30:42 +0100

    Make test pass in Windows

--- micronaut-maven-integration-tests/src/it/issue1005/verify.groovy
+++ micronaut-maven-integration-tests/src/it/issue1005/verify.groovy
@@ -1,4 +1,5 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert !log.text.contains("Cannot find Micronaut Test Resources service settings, server may already be shutdown.")
-assert log.text.startsWith("0.1\nRunning post-build script")
+String text = log.text
+text.readLines().first() == "0.1"
