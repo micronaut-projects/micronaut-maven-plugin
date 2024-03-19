9ebd65407e985544b8e61c3520cb450babdcb2cb
alvarosanchez <alvarosanchez@users.noreply.github.com>
2024-03-19 08:52:32 +0000

    Deploying to gh-pages - 08:52:32

--- snapshot/testapidocs/javadoc.sh
+++ snapshot/testapidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/graalvm-community-jdk-17.0.9_linux-x64_bin/17.0.9/x64/graalvm-community-openjdk-17.0.9+9.1/bin/javadoc -J-Xmx512m -J-Xms128m @options @packages
+/opt/hostedtoolcache/graalvm-jdk-21_linux-x64_bin/21.0.0/x64/graalvm-jdk-21.0.2+13.1/bin/javadoc -J-Xmx512m -J-Xms128m @options @packages
