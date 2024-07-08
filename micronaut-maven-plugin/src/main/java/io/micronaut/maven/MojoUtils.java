82dafd36c898e10a010e703b4f3ca4c3a28570e3
Lahoucine EL ADDALI <lahoucineeladdali@gmail.com>
2024-07-08 10:43:12 +0100

    fix missing package declaration

--- micronaut-maven-plugin/src/main/java/io/micronaut/maven/MojoUtils.java
+++ micronaut-maven-plugin/src/main/java/io/micronaut/maven/MojoUtils.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package io.micronaut.maven;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.maven.execution.MavenSession;
