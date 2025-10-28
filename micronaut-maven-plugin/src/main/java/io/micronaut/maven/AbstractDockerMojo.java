5f8ed588b61ec9f47aa364a5e559efe35b7e9cb5
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2025-10-28 15:38:10 +0100

    Add custom IT

--- micronaut-maven-plugin/src/main/java/io/micronaut/maven/AbstractDockerMojo.java
+++ micronaut-maven-plugin/src/main/java/io/micronaut/maven/AbstractDockerMojo.java
@@ -135,11 +135,15 @@ public abstract class AbstractDockerMojo extends AbstractMicronautMojo {
     protected String networkMode;
 
     /**
+     * <p>
      * jib-maven-plugin goal used to build the Docker image (when Jib is used). Defaults to <code>dockerBuild</code>,
-     * but can be set to <code>build</code> to build the image without a Docker daemon. Note that Jib's <code>build</code>
-     * goal with also push the image to a container registry. Check the
+     * but can be set to <code>build</code> or <code>buildTar</code> to build the image without a Docker daemon.
+     * </p>
+     * <p>
+     * Note that Jib's <code>build</code> goal with also push the image to a container registry. Check the
      * <a href="https://github.com/GoogleContainerTools/jib/tree/master/jib-maven-plugin#quickstart">jib-maven-plugin</a>
      * documentation for more information.
+     * </p>
      *
      * @since 4.11.2
      */
