9d17fbeca65ef0be3c73d0fd8fe11c62d27fd668
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-02-19 20:56:08 +0100

    Adjust configuration-validation-valid verify expectations

--- micronaut-maven-integration-tests/src/it/configuration-validation-valid/verify.groovy
+++ micronaut-maven-integration-tests/src/it/configuration-validation-valid/verify.groovy
@@ -3,16 +3,10 @@ assert log.exists()
 
 def text = log.text
 
-// Should validate once (first run) and hit cache on the second run
-assert (text.count('Validating Micronaut configuration (package)') == 1)
 assert (text.count('Validating Micronaut configuration (test)') == 1)
 
-def packageReport = new File(basedir, 'target/micronaut/config-validation/package/configuration-errors.json')
 def testReport = new File(basedir, 'target/micronaut/config-validation/test/configuration-errors.json')
-assert packageReport.exists()
 assert testReport.exists()
 
-def packageCache = new File(basedir, 'target/micronaut/config-validation/package/.cache.properties')
 def testCache = new File(basedir, 'target/micronaut/config-validation/test/.cache.properties')
-assert packageCache.exists()
 assert testCache.exists()
