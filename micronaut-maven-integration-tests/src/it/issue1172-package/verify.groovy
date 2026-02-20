611770b129a8535c047267ee4e87fca5f51b1b43
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-02-20 12:32:45 +0100

    Remove issue1172-package integration test module

--- micronaut-maven-integration-tests/src/it/issue1172-package/verify.groovy
+++
@@ -1,5 +0,0 @@
-File log = new File(basedir, 'build.log')
-assert log.exists()
-def text = log.text
-assert text.contains("BUILD SUCCESS") : "Docker packaging should succeed"
-assert !text.contains("Could not build image") : "Base image resolution should not fail"
