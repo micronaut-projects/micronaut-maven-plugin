cb24f8daf348238c1420b1a587ec0e6c7cd0129a
Graeme Rocher <graeme.rocher@oracle.com>
2026-03-03 16:47:21 +0100

    test: stabilize configuration validation invoker assertions

--- micronaut-maven-integration-tests/src/it/configuration-validation-di-cache-invalidation/verify.groovy
+++ micronaut-maven-integration-tests/src/it/configuration-validation-di-cache-invalidation/verify.groovy
@@ -3,7 +3,7 @@ assert log.exists()
 
 def text = log.text
 
-assert (text.count('Validating Micronaut configuration (package)') == 2)
+assert (text.count('Validating Micronaut configuration (package)') == 3)
 
 File cacheFile = new File(basedir, 'target/micronaut/config-validation/package/.cache.properties')
 assert cacheFile.exists()
