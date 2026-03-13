40011c899f83204360b8294a27dd257e3c262418
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-03-13 18:52:36 +0100

    Fix configuration validation integration test

--- micronaut-maven-integration-tests/src/it/configuration-validation-options/verify.groovy
+++ micronaut-maven-integration-tests/src/it/configuration-validation-options/verify.groovy
@@ -7,7 +7,7 @@ def text = log.text
 
 // cacheEnabled=false => validation should run on both invocations
 assert (text.count('Validating Micronaut configuration (package)') == 2)
-assert (text.count('Validating Micronaut configuration (test)') == 3)
+assert (text.count('Validating Micronaut configuration (test)') == 2)
 
 File packageJson = new File(basedir, 'target/validation-reports/package/configuration-errors.json')
 File packageHtml = new File(basedir, 'target/validation-reports/package/configuration-errors.html')
@@ -16,7 +16,11 @@ assert !packageHtml.exists()
 
 def packageJsonData = new JsonSlurper().parseText(packageJson.text)
 assert packageJsonData instanceof Map
-assert packageJsonData.isEmpty()
+assert packageJsonData.configurationErrors instanceof List
+assert packageJsonData.configurationErrors.size() == 1
+assert packageJsonData.configurationErrors[0].property == 'micronaut.server.port'
+assert packageJsonData.configurationErrors[0].type == 'WARNING'
+assert packageJsonData.configurationErrors[0].originLocation == 'application-prod.yml'
 
 File testHtml = new File(basedir, 'target/validation-reports/test/configuration-errors.html')
 File testJson = new File(basedir, 'target/validation-reports/test/configuration-errors.json')
