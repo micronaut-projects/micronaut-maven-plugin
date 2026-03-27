1c86e37776c21c71fdecf3ee744bb8ca7d24a6bc
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-03-27 07:57:41 +0100

    test: fix Windows docker-down invoker assertions

--- micronaut-maven-integration-tests/src/it/package-docker-down/verify.groovy
+++ micronaut-maven-integration-tests/src/it/package-docker-down/verify.groovy
@@ -1,3 +1,6 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
-assert log.text.contains("Cannot connect to the Docker daemon at tcp://localhost:65432. Is the docker daemon running?")
+String text = log.text
+assert text.contains("localhost:65432")
+assert text.contains("Cannot connect to the Docker daemon at tcp://localhost:65432. Is the docker daemon running?") ||
+    (text.contains("Failed to read output of 'docker info'") && text.contains("error during connect: Get \"http://localhost:65432/"))
