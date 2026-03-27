6fb4004418e87d2059c158d8f1fa3e2064f8ec50
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-03-27 11:13:53 +0100

    Restore Sisu rule indexing for CheckSnakeYaml

--- micronaut-maven-integration-tests/src/it/snakeyaml-present/verify.groovy
+++ micronaut-maven-integration-tests/src/it/snakeyaml-present/verify.groovy
@@ -1,4 +1,4 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.text.contains("BUILD SUCCESS")
-assert log.text.contains("CheckSnakeYaml passed")
+assert log.text.contains("io.micronaut.maven.enforcer.CheckSnakeYaml passed")
