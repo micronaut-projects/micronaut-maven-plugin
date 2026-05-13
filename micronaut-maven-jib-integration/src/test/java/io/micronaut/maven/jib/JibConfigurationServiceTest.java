f7fc3b69288e4b278bdd2e3d69ce93ecebd1b790
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-13 04:02:59 +0000

    chore: enable checkstyle wildcard import rule

--- micronaut-maven-jib-integration/src/test/java/io/micronaut/maven/jib/JibConfigurationServiceTest.java
+++ micronaut-maven-jib-integration/src/test/java/io/micronaut/maven/jib/JibConfigurationServiceTest.java
@@ -22,7 +22,9 @@ import java.io.StringReader;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
